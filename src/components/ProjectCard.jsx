99bb0cb256caca7b01f5ec703de64951393f2394
Nelson Dela Torre <delatorrenelson127@gmail.com>
2023-01-24 21:43:56 +0800

    project section..

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 export default function ProjectCard({ project }) {
-  const { imgSrc, description, title } = project;
+  const { imgSrc, description, title, href } = project;
 
-  const openLink = () => {
-    const url = `www.google.com`;
+  const openLink = (url) => {
+    // const url = `www.google.com`;
     window.open(url, "_blank");
   };
   return (
-    <div className="col-sm-6 col-lg-3 rounded-5 mb-4">
-      <div className="card shadow bg-body" onClick={openLink}>
+    <div className="col-sm-6 col-lg-3 rounded-3 mb-4">
+      <div className="card shadow bg-body" onClick={()=>{openLink(href)}}>
         <img className="" alt="100%x280" src={imgSrc} />
         <div className="card-body">
           <h4 className="card-title">{title}</h4>
